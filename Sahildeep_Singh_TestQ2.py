83a95d3340c3b89887fdacc5c9e994714eaf802f
12121sahil <90641367+12121sahil@users.noreply.github.com>
2021-10-22 11:12:11 -0400

    Add files via upload

---
+++ Sahildeep_Singh_TestQ2.py
@@ -0,0 +1,11 @@
+#variables
+
+a = input("Write your first name : ",) 
+b = input("Write the First Letter of your Last Name : ")
+c = input("Write your First Name and Last Name And Student ID : ")
+
+#Command for get Output in terminal
+
+print(len(a))
+print(len(b))
+print(len(c))
